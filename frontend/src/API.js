fe1df691f2c4fb00ed2fea6901a31b6a53271e65
jzapata333 <javier.zapata333@gmail.com>
2021-11-18 20:57:29 -0600

    Changes to settings.py

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
-require("dotenv").config();
+require('dotenv').config();
 export const LOGIN_USER_KEY = 'HOME_LOGIN_USER_KEY';
-const {
-    REACT_APP_ENVIRONMENT,
-    REACT_APP_API_BASE_URL_PROD,
-    REACT_APP_API_BASE_URL_DEV,
-  } = process.env;
+const { REACT_APP_ENVIRONMENT, REACT_APP_API_BASE_URL_PROD, REACT_APP_API_BASE_URL_DEV } = process.env;
 
 var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
     baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-    baseURL = 'REACT_APP_API_BASE_URL_DEV';
+    baseURL = 'https://backend-dreamhome-jz333.herokuapp.com/';
 }
 
 const api = axios.create({
@@ -40,9 +36,9 @@ api.interceptors.request.use(
 export default class API {
     // POST API
 
-    sellrequest = async (data) => {
+    sellrequest = async data => {
         console.log('connected to API');
-        
+
         const savedPost = await api
             .post(
                 '/sellrequest/add/',
